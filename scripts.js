774514cf671c879831c8f22a4d2c3bb8f3c781eb
Silvio Souza <silviogome@gmail.com>
2022-02-04 16:30:16 -0300

    :tada: initial commit

---
+++ scripts.js
@@ -0,0 +1,252 @@
+const Modal = {
+    open() {
+        // Abrir modal
+        // Adicionar a class active ao modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .add('active')
+    },
+    close() {
+        // Fechar o modal
+        // Remover a class active do modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .remove('active')
+    }
+}
+
+const Storage = {
+    get() {
+        return JSON.parse(localStorage.getItem("dev.fincances:transactions")) || []
+    },
+
+    set(transactions) {
+        localStorage.setItem("dev.fincances:transactions",
+        JSON.stringify(transactions))
+    }
+}
+
+const Transaction = {
+    all: Storage.get(),
+
+    // Adicionando transações
+    add(transaction){
+        Transaction.all.push(transaction)
+
+        App.reload()
+    },
+    // Removendo transações
+    remove(index) {
+        Transaction.all.splice(index, 1)
+
+
+        App.reload()
+    },
+
+    incomes() {
+        let income = 0;
+        // Pegar todas as transaçoes
+        Transaction.all.forEach(transaction => {
+            if (transaction.amount >0 ) {
+                //Somar a uma variavel e retonar ele
+                income += transaction.amount;
+            }
+        })
+        // Somar as entradas
+        return income
+    },
+
+    expenses() {
+        let expense = 0;
+        // Pegar todas as transaçoes
+        Transaction.all.forEach(transaction => {
+            //valor menor que 0
+            if (transaction.amount < 0 ) {
+                //Somar a uma variavel e retonar ele
+                expense += transaction.amount;
+            }
+        })
+        // Somar as saídas
+        return expense
+    },
+
+    total() {
+        
+        // Entradas - saídas
+        return Transaction.incomes() + Transaction.expenses()
+    }
+}
+
+// Eu preciso pegar as minhas transações do meu objeto aqui no JS e colocar no HTML
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    addTransaciton(transaction, index){
+        
+        const tr= document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index
+
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    
+    innerHTMLTransaction(transaction, index) {
+        const CSSclass= transaction.amount > 0 ? "income" : "expense"
+
+        const aumont= Utils.formatCurrency(transaction.amount)
+
+        const html = `
+             <td class="description">${transaction.description}</td>
+             <td class="${CSSclass}">${aumont}</td>
+             <td class="date">${transaction.date}</td>
+             <td>
+                  <img onclick= "Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
+             </td>
+         `
+
+        return html
+    },
+
+    updateBalance() {
+        document
+        .getElementById('incomeDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.incomes())
+        document
+        .getElementById('expenseDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.expenses())
+        document
+        .getElementById('totalDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.total())
+    },
+
+
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML= ""
+    }
+}
+
+const Utils = {
+    formatDate(date) {
+       const splitttedDate = date.split("-")
+       return `${splitttedDate[2]}/${splitttedDate[1]}/${splitttedDate[0]}`
+    },
+
+    formatAmount(value) {
+        value = Number(value) * 100
+
+        return value
+    },
+    
+    formatCurrency(value) {
+        
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value= String(value).replace(/\D/g, "")
+
+        value= Number(value) /100
+
+        value = value.toLocaleString("pt-BR",{
+            style:"currency",
+            currency: "BRL"
+        })
+       
+        return signal + value
+    }
+}
+
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+
+    getValues () {
+        return{
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+
+    validateField () { //Validação dos campos
+        const {description, amount, date}= Form.getValues()
+        
+
+        if (
+            description.trim() === "" || 
+            amount.trim() === "" || 
+            date.trim() === "" ) {
+                throw new Error("Por favor, preencha todos os campos")
+        }
+    },
+
+    formatValues() {
+        let {description, amount, date}= Form.getValues()
+
+        amount = Utils.formatAmount(amount)
+        date = Utils.formatDate(date)
+
+        return {
+            description,
+            amount,
+            date
+        }
+
+    },
+
+    saveTransaction(transaction) {
+        Transaction.add(transaction)
+    },
+
+    clearFildes() {
+        Form.description.value= ""
+        Form.amount.value= ""
+        Form.date.value= ""
+
+    },
+
+    submit(event) {
+        event.preventDefault()
+        try {
+            
+        // Verificar se todas as informações foram preenchidas
+        Form.validateField()
+        // Formatar os dados para salvar
+        const transaction= Form.formatValues()
+        // Salvar
+        Form.saveTransaction(transaction)
+        // apagar os dados do formulário
+        Form.clearFildes()
+        // modal feche
+        Modal.close()
+
+        } catch (error) {
+            alert(error.message)
+        }
+
+    }
+}
+
+
+const App = {
+    init () {
+
+        Transaction.all.forEach(DOM.addTransaciton) 
+           
+        DOM.updateBalance()
+
+        Storage.set(Transaction.all)
+
+    },
+    reload() {
+        DOM.clearTransactions()
+        App.init()
+    },
+}
+
+App.init()
+
+//Transaction.remove()
